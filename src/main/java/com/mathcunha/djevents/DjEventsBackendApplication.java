eee2beeca1865adc85403a0d30c7b5ab47d6ca42
Matheus Cunha <mathcunha@gmail.com>
2021-11-24 13:29:48 +0000

    first commit

---
+++ src/main/java/com/mathcunha/djevents/DjEventsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mathcunha.djevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjEventsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjEventsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathcunha/djevents/DjEventsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathcunha.djevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjEventsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
